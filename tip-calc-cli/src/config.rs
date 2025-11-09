3ff06caef898517527d9883ac80dd669704f6487
Rushabh Doshi <doshi@Rushabhs-MacBook-Pro.local>
2025-11-09 15:43:41 -0500

    added config skeleton of tip and people

--- tip-calc-cli/src/config.rs
+++ tip-calc-cli/src/config.rs
@@ -3,3 +3,23 @@ pub struct Config {
     pub tip_percentage: u8,
     pub number_of_people: u8,
 }
+
+impl Config {
+    pub fn build(mut args: impl Iterator<Item = String> ) -> Result<Config, &'static str> {
+        args.next();
+        
+        let bill_amount = match args.next() {
+            Some(val) => val.parse::<f32>().map_err(|_| { "Value must be a float." })?,
+            None => return Err("Invalid query string"),
+        };
+
+        let tip_percentage: u8 = 8;
+        let number_of_people: u8 = 8;
+
+        Ok(Config {
+            bill_amount,
+            tip_percentage,
+            number_of_people,
+        })
+    }
+}
