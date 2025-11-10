fc67f6ea55559c1e07ca81ad2fa3389daf24c1a2
Rushabh Doshi <doshi@Rushabhs-MacBook-Pro.local>
2025-11-10 11:48:59 -0500

    init

--- temp-logger/src/config.rs
+++ temp-logger/src/config.rs
@@ -1,5 +1,25 @@
 pub struct Config {
     pub degrees: f32,
-    pub temperature: char,
+    pub temperature: String,
 }
 
+impl Config {
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+
+        let degree = match args.next() {
+            Some(degree) => degree.parse::<f32>().map_err(|_| { "Degrees is not a valid flot."})?,
+            None => return Err("Invalid query."),
+        };
+
+        let temp = match args.next() {
+            Some(temp) => temp,
+            None => return Err("Invalid query."),
+        };
+
+        Ok(Config { 
+            degrees: degree, 
+            temperature: temp,
+        })
+    }
+}
