4bc53295be34ababc95d5910b013815ac14551fe
Rushabh Doshi <doshi@Rushabhs-MacBook-Pro.local>
2025-11-10 15:30:39 -0500

    temp logger logic finishied

--- temp-logger/src/config.rs
+++ temp-logger/src/config.rs
@@ -1,25 +1,53 @@
+pub enum Mode {
+    Log { degree: f32, unit: char },
+    Show,
+}
+
 pub struct Config {
-    pub degrees: f32,
-    pub temperature: String,
+    pub mode: Mode,
 }
 
 impl Config {
     pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
         args.next();
-
-        let degree = match args.next() {
-            Some(degree) => degree.parse::<f32>().map_err(|_| { "Degrees is not a valid flot."})?,
-            None => return Err("Invalid query."),
+        
+        let mode_str = match args.next() {
+            Some(m) => m,
+            None => return Err("No mode provided"),
         };
 
-        let temp = match args.next() {
-            Some(temp) => temp,
-            None => return Err("Invalid query."),
+        let mode = match mode_str.as_str() {
+            "log" => {
+                // parse degree
+                let degree = match args.next() {
+                    Some(d) => d.parse::<f32>().map_err(|_| { "Degree must be a float" })?,
+                    None => return Err("No degree provided"),
+                };
+
+                let unit = match args.next() {
+                    Some(c) => {
+                        let ch = c.chars().next().ok_or("Unit must be a single character.")?;
+                        match ch {
+                            'C' | 'c' | 'F' | 'f' => ch,
+                            _ => return Err("Invalid unit. Must be 'C' or 'F'."),
+                        }
+                    },
+                    None => return Err("No unit provided"),
+                };
+
+                Mode::Log {
+                    degree,
+                    unit
+                }
+            },
+            "show" => {
+                Mode::Show
+            },
+            _ => return Err("Invalid mode. Must be 'log' or 'show'"),
         };
 
-        Ok(Config { 
-            degrees: degree, 
-            temperature: temp,
+        Ok(Config {
+            mode
         })
     }
 }
--- temp-logger/src/main.rs
+++ temp-logger/src/main.rs
@@ -1,5 +1,16 @@
+use std::{env, process};
+
+use crate::{config::Config, temp_logger::run};
+
 pub mod config;
+pub mod temp_logger;
 
 fn main() {
-    println!("Hello, world!");
+    let config = Config::build(env::args())
+        .unwrap_or_else(|err| {
+            eprint!("{}", err);
+            process::exit(1)
+        });
+    
+    run(&config);
 }
