8c020cf4b9bb6620b68fba302f0a566c93a95065
Rushabh Doshi <doshi@Rushabhs-MacBook-Pro.local>
2025-11-11 16:48:59 -0500

    init

--- word-counter/src/main.rs
+++ word-counter/src/main.rs
@@ -1,3 +1,13 @@
+use std::{env, process};
+
+use crate::config::Config;
+
+pub mod config;
 fn main() {
-    println!("Hello, world!");
+    let config = Config::build(env::args())
+        .unwrap_or_else(|err| {
+            eprint!("{}", err);
+            process::exit(1);
+        });
+    
 }
