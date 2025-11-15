8007e8ff42807ec1c84ea6ae3f0188dbe7360ed1
Doshi <rush3doshi@gmail.com>
2025-11-14 22:11:47 -0500

    Feature/to do (#5)

--- to-do-cli/src/main.rs
+++ to-do-cli/src/main.rs
@@ -1,3 +1,19 @@
+use std::{env, process};
+
+use crate::{conifg::Config, task::run};
+
+pub mod task;
+pub mod conifg;
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::build(env::args())
+        .unwrap_or_else(|e: &'static str| {
+            eprintln!("{}", e);
+            process::exit(1);
+        });
+
+    if let Err(e) = run(&config) {
+        eprintln!("Error running Application. {}", e);
+        process::exit(1);
+    }
 }
