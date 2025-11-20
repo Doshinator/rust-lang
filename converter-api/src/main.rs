aaa5029beb6ccfc9ece3a2e0b21be151160cdf3b
Rushabh Doshi <doshi@mac.mynetworksettings.com>
2025-11-19 21:40:24 -0500

    added request and response structs

--- converter-api/src/main.rs
+++ converter-api/src/main.rs
@@ -1,3 +1,41 @@
+// POST/calculate 
+// GET /convert?from=km&to=miles&value=100
+
+use actix_web::web;
+use serde::{Deserialize, Serialize};
+
+// POST/calculate
+#[derive(Debug, Deserialize)]
+struct CalculateRequest {
+    operation: String,
+    a: f64,
+    b: f64,
+}
+
+#[derive(Debug, Serialize)]
+struct CalculateReponse {
+    operation: String,
+    a: f64,
+    b: f64,
+    result: f64,
+}
+
+// For GET /convert?from=km&to=miles&value=100
+#[derive(Debug, Deserialize)]
+struct ConvertQuery {
+    from: String,
+    to: String,
+    value: f64,
+}
+
+#[derive(Debug, Serialize)]
+struct ConvertQueryResponse {
+    from: String,
+    to: String,
+    original_value: f64,
+    converted_value: f64,
+}
+
 fn main() {
     println!("Hello, world!");
 }
