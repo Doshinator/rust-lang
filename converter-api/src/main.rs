81aed440e1c443e260188d89a5ecc986592fdc14
Rushabh Doshi <doshi@Rushabhs-MacBook-Pro.local>
2025-11-21 12:47:33 -0500

    post/calculate done

--- converter-api/src/main.rs
+++ converter-api/src/main.rs
@@ -1,7 +1,7 @@
 // POST/calculate 
 // GET /convert?from=km&to=miles&value=100
 
-use actix_web::web;
+use actix_web::{HttpResponse, Result, web};
 use serde::{Deserialize, Serialize};
 
 // POST/calculate
@@ -34,6 +34,32 @@ struct ConvertQueryResponse {
     to: String,
     original_value: f64,
     converted_value: f64,
+}
+
+// POST/calculate
+async fn calculate(body: web::Json<CalculateRequest>) -> Result<HttpResponse> {
+    let result = match body.operation.as_str() {
+        "add" => body.a + body.b,
+        "subtract" => body.a - body.b,
+        "divide" => {
+            if body.b == 0.0 {
+                return Ok(HttpResponse::BadRequest().json(serde_json::json!({
+                    "error" : "can not have divisor as 0"
+                })));
+            }
+            body.a / body.b
+        },
+        _ => return Ok(HttpResponse::BadRequest().json(serde_json::json!({
+            "error": "Unknown operation. Use: add, subtract, multiply, divide"
+        }))),
+    };
+
+    Ok(HttpResponse::Ok().json(CalculateReponse {
+        operation: body.operation.clone(),
+        a: body.a,
+        b: body.b,
+        result,
+    }))
 }
 
 fn main() {
