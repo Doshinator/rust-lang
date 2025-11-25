9302e8ff7f86bde848387250418b716a13544fb8
Rushabh Doshi <doshi@mac.mynetworksettings.com>
2025-11-24 19:07:58 -0500

    model setup

--- url-shortener/src/main.rs
+++ url-shortener/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+// POST /shorten
+
+// GET/{short_url}
+
+use actix_web::{App, HttpServer};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Debug)]
+struct ShortenRequest {
+    url: String,
+}
+
+#[derive(Debug, Serialize)]
+struct ShortenResponse {
+    short_code: String,
+    short_url: String,
+}
+
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(||
+        App::new()
+    )
+    .bind(("127.0.01", 8080))?
+    .run()
+    .await
 }
