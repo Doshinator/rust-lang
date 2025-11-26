42ecf729e7a6ddfcddb8da8c119be6dcb66835e3
Rushabh Doshi <doshi@mac.mynetworksettings.com>
2025-11-25 21:54:12 -0500

    update and delete done

--- notes-api/src/main.rs
+++ notes-api/src/main.rs
@@ -26,7 +26,7 @@ struct CreateNoteRequest {
 }
 
 #[derive(Debug, Deserialize)]
-struct UpdateNotRequest {
+struct UpdateNoteRequest {
     title: Option<String>,
     content: Option<String>,
 }
@@ -73,6 +73,51 @@ async fn get_note_id(
         None => Ok(HttpResponse::NotFound().json(serde_json::json!({
             "error" : "Note not found"
         }))),
+    }
+}
+
+// PUT /notes/{id} - Update a note
+async fn update_note(
+    state: web::Data<AppState>,
+    id: web::Path<Uuid>,
+    body: web::Json<UpdateNoteRequest>,
+) -> Result<HttpResponse> {
+    let mut notes = state.notes.lock().unwrap();
+    match notes.iter_mut().find(|n| n.id == *id) {
+        Some(note) => {
+            if let Some(title) = &body.title {
+                note.title = title.clone();
+            }
+
+            if let Some(content) = &body.content {
+                note.content = content.clone();
+            }
+            note.updated_at = Utc::now();
+            
+            Ok(HttpResponse::Ok().json(note))
+        },
+        None => Ok(HttpResponse::NotFound().json(serde_json::json!({
+            "error" : "Note not found"
+        }))),
+    }
+}
+
+// DELETE /notes/{id} 
+async fn delete_note(
+    state: web::Data<AppState>,
+    id: web::Path<Uuid>,
+) -> Result<HttpResponse> {
+    let mut notes = state.notes.lock().unwrap();
+    let original_len = notes.len();
+    notes.retain(|n| n.id != *id);
+
+    if notes.len() < original_len {
+        Ok(HttpResponse::NoContent().finish())
+    }
+    else {
+        Ok(HttpResponse::NotFound().json(serde_json::json!({
+            "error" : "Note not found"
+        })))
     }
 }
 
