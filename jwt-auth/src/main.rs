e36c8eba44da49963fca72b12405d7a037daada1
Rushabh Doshi <doshi@Rushabhs-MacBook-Pro.local>
2025-12-13 20:33:47 -0500

    update model added

--- jwt-auth/src/main.rs
+++ jwt-auth/src/main.rs
@@ -3,8 +3,9 @@
 // PUT /accounts/{id} - Update account
 // DELETE /accounts/{id} - Delete account
 
+use actix_web::{App, HttpServer, web, Result};
 use serde::{Deserialize, Serialize};
-use sqlx::prelude::FromRow;
+use sqlx::{PgPool, prelude::FromRow};
 use uuid::Uuid;
 
 #[derive(Debug, Serialize, Deserialize, FromRow)]
@@ -20,8 +21,36 @@ struct GameAccount {
 struct CreateGameAccountRequest {
     username: String,
     platform: String,
+    level: Option<i32>,
+    total_hours_played: Option<i32>,
 }
 
-fn main() {
-    println!("Hello, world!");
+struct UpdateGameAccount {
+    username: Option<String>,
+    platform: Option<String>,
+    level: Option<i32>,
+    total_hours_played: Option<i32>,    
+}
+
+struct AppState {
+    db: PgPool,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let db_url = "postgres://postgres:postgres@localhost/game_account_db";
+    let pool = PgPool::connect(db_url)
+        .await
+        .expect("Failed to connect to db");
+
+    let app_state = web::Data::new(AppState {db: pool});
+
+    HttpServer::new(move || {
+        App::new()
+        .app_data(app_state.clone())
+        
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
