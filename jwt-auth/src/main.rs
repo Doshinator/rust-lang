5bcefcbc78bf9bdb49f61fa3b24bb6bb4dae7dff
Rushabh Doshi <doshi@Rushabhs-MacBook-Pro.local>
2025-12-13 20:20:00 -0500

    model added

--- jwt-auth/src/main.rs
+++ jwt-auth/src/main.rs
@@ -1,3 +1,27 @@
+// POST /accounts - Add game account
+// GET /accounts - List all accounts
+// PUT /accounts/{id} - Update account
+// DELETE /accounts/{id} - Delete account
+
+use serde::{Deserialize, Serialize};
+use sqlx::prelude::FromRow;
+use uuid::Uuid;
+
+#[derive(Debug, Serialize, Deserialize, FromRow)]
+struct GameAccount {
+    id: Uuid,
+    username: String,
+    platform: String,
+    level: i32,
+    total_hours_played: i32,
+}
+
+#[derive(Debug, Deserialize)]
+struct CreateGameAccountRequest {
+    username: String,
+    platform: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
